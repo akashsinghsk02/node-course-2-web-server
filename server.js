44271ac36338da476b82ce476c847c7e057334ec
Aakash <akashsinghsk02@gmail.com>
2019-01-30 19:17:06 +0530

    test1

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
    fs.appendFile('server.log',log+'\n',(err)=>{
       if(err)
       console.log('unable to add data to file Error message ='+err);
-   });
+   });  
    next(); 
 })
 
